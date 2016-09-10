da36d777b6acbb9c04dec107a66eef4fd91d79b9
piotermiarer <piotr@atopik.pl>
2016-09-10 22:38:36 +0200

    Place view now passes an availability calendar to a render function and place template displays it in a very simple way.

--- boiska/views.py
+++ boiska/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from calendar import Calendar
+from datetime import datetime
 
 from .models import Place
 
+from random import randint
 
 def index(request):
     """
@@ -17,11 +20,40 @@ def place(request, place_name):
     TODO: Calendar showing availability of pitches/courts.
     """
     place_obj = get_object_or_404(Place, name=place_name)
+    now = datetime.now()
+    my_calendar = availability_calendar(now.year, now.month, place_obj)
     context = {
         'name': place_name,
         'description': place_obj.description,
         'phone_number': place_obj.phone_number,
         'city': place_obj.city,
         'street': place_obj.street,
+        'calendar': my_calendar,
     }
     return render(request, 'boiska/place.html', context)
+
+def availability_calendar(year, month, place_obj):
+    """
+    Availability calendar returns a list of week lists.
+    Each week list contains tuples in format:
+    (month_day, week_day, availability).
+    TODO: Availability values:
+     - 0 -> almost every hour is available
+     - 1 -> a pitch/court is quite busy
+     - 2 -> very busy
+    """
+    result = []
+    my_calendar = Calendar()
+    for day in my_calendar.itermonthdays2(year, month):
+        # create a list for a new week
+        if day[1] == 0:
+            result.append([])
+        availability = check_availability(year, month, day, place_obj)
+        result[-1].append((day[0], day[1], availability))
+    return result
+
+def check_availability(year, month, day, place_obj):
+    """
+    TODO: Check availability of place's courts on a particuar day.
+    """
+    return randint(0,2)
